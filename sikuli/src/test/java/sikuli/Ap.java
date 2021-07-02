ede13fda131ed16cac58bfaf5c894a27425f65ad
user <achievekogul231@gmail.com>
2021-07-02 19:21:09 +0530

    Test commit

--- sikuli/src/test/java/sikuli/Ap.java
+++ sikuli/src/test/java/sikuli/Ap.java
@@ -5,7 +5,7 @@ import org.sikuli.script.App;
 public class Ap {
 public static void main(String[] args) throws InterruptedException {
 	
-	
+	System.out.println();
 	App.open("C:\\Windows\\System32\\calc.exe");
 	Thread.sleep(1000);
 	
